f82aa4f4c4041092eba2f4ebd8f5ac452bb3a71f
manuraj1107 <manurajrockstar2000@gmail.com>
2022-10-15 23:19:50 +0530

    Created the basic structure of the project's folder, files and modules

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Autocomplete} from '@react-google-maps/api';
+import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
